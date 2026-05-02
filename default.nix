208c9699cddb81d018f1e52b7bdf44bd42824fc0
JuxGD <jak@e.email>
2026-05-01 21:28:09 -0400

    oh yeah we don't need that

--- default.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import <nixpkgs> { } }:
-
-{
-  dw-proton = pkgs.callPackage ./dw-proton.nix {};
-  ge-proton = pkgs.callPackage ./ge-proton.nix {};
-  cachyos-proton = pkgs.callPackage ./cachyos-proton.nix {};
-}
