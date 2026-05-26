a6708cbae0358627e851868d8d15d4c5c333fc06
JuxGD <117054307+JuxGD@users.noreply.github.com>
2026-05-26 04:04:46 +0000

    Update cachyos-proton to 11.0-20260519-slr

--- cachyos-proton.nix
+++ cachyos-proton.nix
@@ -5,11 +5,11 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "proton-cachyos";
-  version = "11.0-20260506-slr";
+  version = "11.0-20260519-slr";
 
   src = fetchzip {
     url = "https://github.com/CachyOS/${finalAttrs.pname}/releases/download/cachyos-${finalAttrs.version}/proton-cachyos-${finalAttrs.version}-x86_64_v3.tar.xz";
-    hash = "sha256-eFj1eZj+aMkZyUFdcB9OdLQrrTukbaBBTlLZkwq9xNs=";
+    hash = "sha256-MDVGzDVLjj+duMGP8wXwtRORu8KP2vIAt6orHbVvHpk=";
   };
 
   dontUnpack = true;
